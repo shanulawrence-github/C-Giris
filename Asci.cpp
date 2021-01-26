ff570304dd6f44c035b8dbfe42f60d7914a2c6e0
Yasin Yavuz <yyavuz.yasinn15@gmail.com>
2021-01-26 21:40:00 +0300

    C/C++ Giris-9

---
+++ Asci.cpp
@@ -0,0 +1,15 @@
+//
+// Karakterlerin sayýsal karþýlýklarý
+#include <stdio.h>
+
+// main fonk baslar
+int main(void){
+	
+	// bilgisayarlarýn harfleri ve özel karakterleri direk olarak bulunduramaz bunun için buna karþýlýk gelen
+	// rakamsal deðerleri vardýr bunun standartlaþtýrýlmasý(ASCI) ile her bilgisayarda belli bir harfin veya karakterin
+	// sayýsal bir karþýlýðý vardýr
+	
+	printf("%d\n",'a'); // bu þekilde karakterlerin sayýsal karþýlýklarýný bulabiliriz
+	printf("%d\n",'/'); // bu þekilde özel karakterlerinde sayýsal karþýlýklarýný bulabiliriz
+	 
+}// main fonk biter
